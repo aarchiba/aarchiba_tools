edcbe4ed296445c30a3dba67ce038ae5d4bf163d
Anne Archibald <archibald@astron.nl>
2019-07-19 19:23:15 +0200

    Basic project layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aarchiba_tools
+
 Handy python (3) tools I often use
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aarchiba_tools",
+    version="0.0.1",
+    author="Anne Archibald",
+    author_email="peridot.faceted@gmail.com",
+    description="Convenient python (3) tools I often use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aarchiba/aarchiba_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
