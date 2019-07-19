94effd3df0196a4d42f3fe5f8e0be85b197712db
Anne Archibald <archibald@astron.nl>
2019-07-19 21:57:16 +0200

    Got installation working
    Using tox to test installation and operation on 2.7 and 3.7

--- setup.py
+++ setup.py
@@ -8,17 +8,21 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
     name="aarchiba_tools",
-    version="0.0.1",
+    version="0.1",
     author="Anne Archibald",
     author_email="peridot.faceted@gmail.com",
     description="Convenient python (3) tools I often use",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aarchiba/aarchiba_tools",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("src"),
+    package_dir={'': 'src'},
+    keywords=['personal'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['numpy'],
+    include_package_data=True,
 )
