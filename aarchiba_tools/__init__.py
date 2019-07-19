3c418a28003a746f9c7917209246c53b0f1693c0
Anne Archibald <archibald@astron.nl>
2019-07-19 20:39:37 +0200

    Implemented first two functions

--- aarchiba_tools/__init__.py
+++ aarchiba_tools/__init__.py
@@ -1,3 +1,85 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-name = "aarchiba_tools"
+import numpy as np
+
+
+def downsample(a, factor, axis=-1, func=np.mean):
+    """Return the original array downsampled along a particular axis.
+
+    Groups the specified axis into blocks of size `factor` and applies
+    the provided `func` (by default the mean, but sum, product and
+    maxiumum are sensible too) to the axis of blocks.
+
+    Parameters
+    ----------
+    a : array_like
+        The array to be manipulated.
+    factor : int
+        The factor by which to downsample the array. The relevant axis
+        should have a length exactly divisible by factor.
+    axis : int, optional
+        The axis to apply the operation to.
+    func : callable
+        The function to apply. Should accept an array as input and an
+        axis argument; will be called with a newly-created axis of length
+        factor, and it should return a result with this axis removed.
+
+    Returns
+    -------
+    ad : array type based on type of a
+        The axis specified will have been reduced by a factor `factor`
+        but will still be part of the shape even if it has length 1.
+    """
+    a = np.asanyarray(a)
+    s = np.shape(a)
+    if axis<0:
+        axis += len(s)
+    if axis<0 or axis>=len(s):
+        raise ValueError("Input array has only %d dimensions" % len(s))
+    if factor<=0:
+        raise ValueError("Cannot downsample array by factor %s" % factor)
+    n = s[axis]
+    if n%factor != 0:
+        raise ValueError("Axis length %d not divisible by factor %d"
+                             % (n,factor))
+    ns = s[:axis]+(n//factor,factor)+s[axis+1:]
+    ar = np.reshape(a,ns)
+    return func(ar, axis=axis+1)
+
+def logspace_exp(start, stop, num=50, endpoint=True):
+    """Return logarithmically spaced values between the endpoints.
+
+    Parameters
+    ----------
+    start : float
+        Start value. Should be nonzero.
+    stop : float
+        Stop value. Should be nonzero and have the same sign as start.
+    num : int, optional
+        Number of points to generate.
+    endpoint : bool, optional
+        Whether to include the endpoint.
+
+    Returns
+    -------
+    samples : ndarray
+        There are `num` samples in the closed or open (depending on
+        `endpoint`) interval between `start` and `stop`. Their logarithms
+        are spaced equally between the logarithms of `start` and `stop`.
+
+    Results will always be of dtype float.
+    """
+    if start==0 or stop==0:
+        raise ValueError("start and stop values must be nonzero")
+    if start<0:
+        s = -1
+        start = -start
+        stop = -stop
+    else:
+        s = 1
+    if stop<0:
+        raise ValueError("start and stop values must have the same sign")
+
+    return s*np.exp(np.linspace(np.log(start), np.log(stop),
+                            num=num, endpoint=endpoint, dtype=float))
---
+++ test_all.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from numpy.testing import (assert_, assert_equal,
+                               assert_almost_equal, assert_raises)
+from aarchiba_tools import downsample, logspace_exp
+
+def test_downsample_1d():
+    assert_almost_equal(downsample([1,2,3,4],2),
+                        [1.5,3.5])
+    assert_almost_equal(downsample([1,2,3,4],4),
+                        [2.5])
+    assert_almost_equal(downsample([1,2,3,4],1),
+                        [1,2,3,4])
+
+def test_downsample_raises():
+    assert_raises(ValueError,downsample,[1,2,3,4],3)
+    assert_raises(ValueError,downsample,[1,2,3,4],0)
+    assert_raises(ValueError,downsample,[1,2,3,4],-1)
+
+    assert_raises(ValueError,downsample,np.zeros((2,3)),1,axis=2)
+    assert_raises(ValueError,downsample,np.zeros((2,3)),1,axis=-3)
+
+def test_downsample_negative_axes():
+    a = np.random.randn(6,35,11*13)
+    assert_almost_equal(downsample(a,11,axis=2),
+                        downsample(a,11,axis=-1))
+    assert_almost_equal(downsample(a,3,axis=0),
+                        downsample(a,3,axis=-3))
+
+def test_downsample_shape():
+    a = np.random.randn(6,6,6)
+    assert_equal(downsample(a,3,axis=0).shape,(2,6,6))
+    assert_equal(downsample(a,3,axis=1).shape,(6,2,6))
+    assert_equal(downsample(a,3,axis=2).shape,(6,6,2))
+    assert_equal(downsample(a,3,axis=-3).shape,(2,6,6))
+    assert_equal(downsample(a,3,axis=-2).shape,(6,2,6))
+    assert_equal(downsample(a,3,axis=-1).shape,(6,6,2))
+
+def test_downsample_dimensions():
+    a = np.random.randn(6,6,6)
+    assert_almost_equal(downsample(a,6,axis=0),a.mean(axis=0)[None,:,:])
+    assert_almost_equal(downsample(a,6,axis=1),a.mean(axis=1)[:,None,:])
+    assert_almost_equal(downsample(a,6,axis=2),a.mean(axis=2)[:,:,None])
+
+def test_logspace_exp_basic():
+    assert_almost_equal(logspace_exp(1,1000,4),[1,10,100,1000])
